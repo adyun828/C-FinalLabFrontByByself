009db43ff8958a68c6141bc9e5b175221bc47f1e
adyun <3515843408@qq.com>
2025-12-06 23:40:57 +0800

    在新的 feature 分支上提交覆盖的代码

---
+++ register.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btnClose;
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.regTitle = new System.Windows.Forms.Label();
+            this.reg_panel = new System.Windows.Forms.Panel();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnregister = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.text_password2 = new System.Windows.Forms.TextBox();
+            this.text_password1 = new System.Windows.Forms.TextBox();
+            this.text_user = new System.Windows.Forms.TextBox();
+            btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.reg_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            btnClose.BackColor = System.Drawing.SystemColors.ControlLight;
+            btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnClose.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            btnClose.ForeColor = System.Drawing.SystemColors.HotTrack;
+            btnClose.Image = global::WindowsFormsApp1.Properties.Resources.屏幕截图_2025_12_06_154800;
+            btnClose.Location = new System.Drawing.Point(773, 0);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new System.Drawing.Size(34, 48);
+            btnClose.TabIndex = 3;
+            btnClose.Text = "×";
+            btnClose.UseVisualStyleBackColor = false;
+            btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox.Image = global::WindowsFormsApp1.Properties.Resources.bg_login;
+            this.pictureBox.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(808, 539);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // regTitle
+            // 
+            this.regTitle.AutoSize = true;
+            this.regTitle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.regTitle.Font = new System.Drawing.Font("微软雅黑", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.regTitle.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.regTitle.Image = global::WindowsFormsApp1.Properties.Resources.屏幕截图_2025_12_06_154800;
+            this.regTitle.Location = new System.Drawing.Point(322, 61);
+            this.regTitle.Name = "regTitle";
+            this.regTitle.Size = new System.Drawing.Size(144, 75);
+            this.regTitle.TabIndex = 1;
+            this.regTitle.Text = "注册";
+            // 
+            // reg_panel
+            // 
+            this.reg_panel.Controls.Add(this.btnlogin);
+            this.reg_panel.Controls.Add(this.btnregister);
+            this.reg_panel.Controls.Add(this.checkBox1);
+            this.reg_panel.Controls.Add(this.text_password2);
+            this.reg_panel.Controls.Add(this.text_password1);
+            this.reg_panel.Controls.Add(this.text_user);
+            this.reg_panel.Location = new System.Drawing.Point(234, 167);
+            this.reg_panel.Name = "reg_panel";
+            this.reg_panel.Size = new System.Drawing.Size(311, 332);
+            this.reg_panel.TabIndex = 2;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(33, 276);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(251, 32);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "登录";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnregister
+            // 
+            this.btnregister.Location = new System.Drawing.Point(33, 238);
+            this.btnregister.Name = "btnregister";
+            this.btnregister.Size = new System.Drawing.Size(250, 32);
+            this.btnregister.TabIndex = 4;
+            this.btnregister.Text = "注册";
+            this.btnregister.UseVisualStyleBackColor = true;
+            this.btnregister.Click += new System.EventHandler(this.btnregister_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(32, 194);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 22);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "显示密码";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // text_password2
+            // 
+            this.text_password2.Location = new System.Drawing.Point(32, 151);
+            this.text_password2.Name = "text_password2";
+            this.text_password2.Size = new System.Drawing.Size(252, 28);
+            this.text_password2.TabIndex = 2;
+            this.text_password2.Enter += new System.EventHandler(this.text_password2_GotFocus);
+            this.text_password2.Leave += new System.EventHandler(this.text_password2_LostFocus);
+            // 
+            // text_password1
+            // 
+            this.text_password1.Location = new System.Drawing.Point(32, 92);
+            this.text_password1.Name = "text_password1";
+            this.text_password1.Size = new System.Drawing.Size(252, 28);
+            this.text_password1.TabIndex = 1;
+            this.text_password1.Enter += new System.EventHandler(this.text_password1_GotFocus);
+            this.text_password1.Leave += new System.EventHandler(this.text_password1_LostFocus);
+            // 
+            // text_user
+            // 
+            this.text_user.Location = new System.Drawing.Point(32, 28);
+            this.text_user.Name = "text_user";
+            this.text_user.Size = new System.Drawing.Size(252, 28);
+            this.text_user.TabIndex = 0;
+            this.text_user.TextChanged += new System.EventHandler(this.text_user_TextChanged);
+            this.text_user.Enter += new System.EventHandler(this.text_user_GotFocus);
+            this.text_user.Leave += new System.EventHandler(this.text_user_LostFocus);
+            // 
+            // register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(btnClose);
+            this.Controls.Add(this.reg_panel);
+            this.Controls.Add(this.regTitle);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "register";
+            this.Text = "register";
+            this.Load += new System.EventHandler(this.register_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.reg_panel.ResumeLayout(false);
+            this.reg_panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label regTitle;
+        private System.Windows.Forms.Panel reg_panel;
+        private System.Windows.Forms.TextBox text_password2;
+        private System.Windows.Forms.TextBox text_password1;
+        private System.Windows.Forms.TextBox text_user;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnregister;
+    }
+}
---
+++ register.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class register : Form
+    {
+        // 输入框提示文字
+        private const string UserTip = "请输入用户名";
+        private const string PwdTip1 = "请输入密码";
+        private const string PwdTip2 = "请再次输入密码";
+
+        // 窗体拖拽变量
+        private Point mouseDownPoint;
+
+        public register()
+        {
+            InitializeComponent();
+            // 初始化输入框提示文字
+            InitInputTips();
+        }
+
+        #region 1. 初始化输入框提示文字
+        private void InitInputTips()
+        {
+            // 用户名输入框
+            if (string.IsNullOrEmpty(text_user.Text))
+            {
+                text_user.Text = UserTip;
+                text_user.ForeColor = Color.Gray;
+            }
+            // 密码输入框
+            if (string.IsNullOrEmpty(text_password1.Text))
+            {
+                text_password1.Text = PwdTip1;
+                text_password1.PasswordChar = '\0'; // 初始不隐藏
+                text_password1.ForeColor = Color.Gray;
+            }
+            // 确认密码输入框（注册页特有）
+            if (string.IsNullOrEmpty(text_password2.Text))
+            {
+                text_password2.Text = PwdTip2;
+                text_password2.PasswordChar = '\0';
+                text_password2.ForeColor = Color.Gray;
+            }
+        }
+        #endregion
+
+        #region 2. 输入框焦点事件
+        // 用户名输入框 - 获焦
+        private void text_user_GotFocus(object sender, EventArgs e)
+        {
+            if (text_user.Text == UserTip)
+            {
+                text_user.Text = "";
+                text_user.ForeColor = Color.Black;
+            }
+        }
+        // 用户名输入框 - 失焦
+        private void text_user_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_user.Text))
+            {
+                text_user.Text = UserTip;
+                text_user.ForeColor = Color.Gray;
+            }
+        }
+
+        // 密码输入框 - 获焦
+        private void text_password1_GotFocus(object sender, EventArgs e)
+        {
+            if (text_password1.Text == PwdTip1)
+            {
+                text_password1.Text = "";
+                text_password1.ForeColor = Color.Black;
+                text_password1.PasswordChar = '*'; // 获焦后隐藏
+            }
+        }
+        // 密码输入框 - 失焦
+        private void text_password1_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_password1.Text))
+            {
+                text_password1.Text = PwdTip1;
+                text_password1.ForeColor = Color.Gray;
+                text_password1.PasswordChar = '\0'; // 失焦后显示提示文字
+            }
+        }
+
+        // 确认密码输入框 - 获焦
+        private void text_password2_GotFocus(object sender, EventArgs e)
+        {
+            if (text_password2.Text == PwdTip2)
+            {
+                text_password2.Text = "";
+                text_password2.ForeColor = Color.Black;
+                text_password2.PasswordChar = '*';
+            }
+        }
+        // 确认密码输入框 - 失焦
+        private void text_password2_LostFocus(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_password2.Text))
+            {
+                text_password2.Text = PwdTip2;
+                text_password2.ForeColor = Color.Gray;
+                text_password2.PasswordChar = '\0';
+            }
+        }
+        #endregion
+
+        #region 3. 显示密码复选框事件
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // 密码框1
+            if (text_password1.Text != PwdTip1)
+            {
+                text_password1.PasswordChar = checkBox1.Checked ? '\0' : '*';
+            }
+            // 密码框2
+            if (text_password2.Text != PwdTip2)
+            {
+                text_password2.PasswordChar = checkBox1.Checked ? '\0' : '*';
+            }
+        }
+        #endregion
+
+        #region 4. 按钮点击事件
+        // 注册按钮 - 点击
+        private void btnregister_Click(object sender, EventArgs e)
+        {
+            string userName = text_user.Text.Trim();
+            string pwd1 = text_password1.Text.Trim();
+            string pwd2 = text_password2.Text.Trim();
+
+            // 基础验证
+            if (userName == UserTip || string.IsNullOrEmpty(userName))
+            {
+                MessageBox.Show("请输入用户名！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                text_user.Focus();
+                return;
+            }
+            if (pwd1 == PwdTip1 || string.IsNullOrEmpty(pwd1))
+            {
+                MessageBox.Show("请输入密码！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                text_password1.Focus();
+                return;
+            }
+            if (pwd2 == PwdTip2 || string.IsNullOrEmpty(pwd2))
+            {
+                MessageBox.Show("请再次输入密码！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                text_password2.Focus();
+                return;
+            }
+            if (pwd1 != pwd2)
+            {
+                MessageBox.Show("两次输入的密码不一致！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                text_password2.Focus();
+                return;
+            }
+
+            // 注册成功（后续可对接API）
+            MessageBox.Show("注册成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close(); // 关闭注册页返回登录页
+        }
+
+        // 登录按钮 - 点击（返回登录页）
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // 关闭按钮 - 点击
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确定要关闭吗？", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+        #endregion
+
+        #region 5. 窗体拖拽事件
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(
+                    this.Location.X + e.X - mouseDownPoint.X,
+                    this.Location.Y + e.Y - mouseDownPoint.Y
+                );
+            }
+        }
+        #endregion
+
+        private void register_Load(object sender, EventArgs e)
+        {
+            // 窗体加载事件（暂无逻辑，保留）
+        }
+
+        private void text_user_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
